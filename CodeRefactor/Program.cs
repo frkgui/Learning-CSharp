3db4029bb75fde82a3a95f9a1af11547dbc629ec
Frank <contatoguilhermefrank@gmail.com>
2024-03-23 02:04:40 -0300

    Adicionar arquivos de projeto.

---
+++ CodeRefactor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+static void main (String[] args)
+{
+
+    // Application inital Config Information
+
+    Console.Write("Type the hotel name: ");
+    string hotelName = Console.ReadLine();
+    Console.Write($"Type the numbers of room [{hotelName} have]");
+    int hotelNumberOfRooms = int.Parse(Console.ReadLine());
+    int idGuest = 0;
+
+    Room[] allRooms = new Room[hotelNumberOfRooms];
+    for (int i = 0; i < hotelNumberOfRooms; i++)
+    {
+        allRooms[i] = new Room();
+    }
+
+    
+
+}
+
+ 
